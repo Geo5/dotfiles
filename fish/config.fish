e017ec619deefdad3538153f73bce21ae3b41c43
Geo5 <berta.maler@posteo.de>
2023-09-26 17:19:07 +0200

    Added vscode and ghcup integration

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/georg/.ghcup/bin $PATH # ghcup-env
+
+string match -q "$TERM_PROGRAM" "vscode"
+and . (code --locate-shell-integration-path fish)
